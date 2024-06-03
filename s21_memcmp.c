e9af98134afd139c7ffa121acb9f1443f1ed9375
Maksim <pumpbro@gmail.com>
2024-06-03 22:16:32 +0300

    feat: realise all mem* functions from the task

--- s21_memcmp.c
+++ s21_memcmp.c
@@ -1,10 +1,10 @@
 #include "s21_string.h"
 
-int s21_memcmp(const void *arr1, const void *arr2, s21_size_t n) {
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
   int result = 0;
   while (n > 0 && !result) {
-    int ch1 = *((char *)arr1++);
-    int ch2 = *((char *)arr2++);
+    int ch1 = *((char *)str1++);
+    int ch2 = *((char *)str2++);
     if (ch1 != ch2) result = ch1 - ch2;
     n--;
   }
---
+++ s21_memcpy.c
@@ -0,0 +1,8 @@
+#include "s21_string.h"
+
+void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
+    for (int i = 0; i < n; i++) {
+        ((char *) dest)[i] = ((char *) src)[i];
+    }
+    return dest;
+}
