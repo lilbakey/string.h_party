53b4d69dea38f4c08640ae711c05d8d44d34721b
Tyrion Cassel <tyrionca@us-a1.yar.21-school.ru>
2024-06-04 18:02:09 +0300

    fix: clang

--- s21_memcmp.c
+++ s21_memcmp.c
@@ -5,7 +5,8 @@ int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
   while (n > 0 && !result) {
     int ch1 = *((char *)str1++);
     int ch2 = *((char *)str2++);
-    if (ch1 != ch2) result = ch1 - ch2;
+    if (ch1 != ch2)
+      result = ch1 - ch2;
     n--;
   }
   return result;
--- s21_strchr.c
+++ s21_strchr.c
@@ -1,13 +1,13 @@
 #include "s21_string.h"
 
 char *s21_strchr(const char *str, int symbol) {
-    const char *res = S21_NULL;
-    while (*str) {
-        if (*str == (char)symbol) {
-            res = str;
-            break;
-        }
-        str++;
+  const char *res = S21_NULL;
+  while (*str) {
+    if (*str == (char)symbol) {
+      res = str;
+      break;
     }
-    return (char *)res;
+    str++;
+  }
+  return (char *)res;
 }
