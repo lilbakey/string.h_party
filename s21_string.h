58f5938b6d0fc6446064e1272c9fd0ab528f5769
Tyrion Cassel <tyrionca@us-a1.yar.21-school.ru>
2024-06-04 17:59:37 +0300

    feat: T10D16 copypaste

--- s21_string.h
+++ s21_string.h
@@ -8,5 +8,8 @@ void *s21_memchr(const void *str, int c, s21_size_t n);
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
+s21_size_t s21_strlen(const char *str);
+char *s21_strchr(const char *str, int symbol);
+s21_size_t s21_strcspn(const char *str, const char *sym);
 
 #endif
