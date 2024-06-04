b64526f45af74af992c31437da62f1e78d6dcac7
Tyrion Cassel <tyrionca@us-a1.yar.21-school.ru>
2024-06-04 18:03:23 +0300

    fix: clang

--- s21_strcspn.c
+++ s21_strcspn.c
@@ -4,8 +4,7 @@ s21_size_t s21_strcspn(const char *str, const char *sym) {
   s21_size_t length = s21_strlen(str);
   for (s21_size_t i = 0; i < s21_strlen(sym); i++) {
     for (s21_size_t j = 0; j < length; j++) {
-      if (str[j] == sym[i])
-        length = j;
+      if (str[j] == sym[i]) length = j;
     }
   }
   return length;
