75ad62c00bed82b4d7a2717d687200f948c29154
lilbakey <alexbakeev07@gmail.com>
2024-06-05 02:30:31 +0300

    add s21_strpbrk func

--- s21_string.h
+++ s21_string.h
@@ -17,5 +17,6 @@ int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strerror(int errnum);
 char *s21_strrchr(const char *str, int c);
 char *s21_strstr(const char *haystack, const char *needle);
+char *s21_strpbrk(const char *str1, const char *str2);
 
 #endif
---
+++ s21_strpbrk.c
@@ -0,0 +1,11 @@
+#include "s21_string.h"
+
+char *s21_strpbrk(const char *str1, const char *str2) {
+  char *result = s21_NULL;
+  char *temp = (char *)str1;
+  s21_size_t length = strcspn(str1, str2);
+  for (s21_size_t i = 0; i < length; i++) temp++;
+  if (length) result = temp;
+
+  return result;
+}
