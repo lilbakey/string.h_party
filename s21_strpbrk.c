2a7885aac634f1fc62005f5f00fe8235e0a2246a
Ytrams Septic <ytramsse@us-b4.yar.21-school.ru>
2024-06-05 17:58:44 +0300

    hot fix

--- s21_strpbrk.c
+++ s21_strpbrk.c
@@ -3,7 +3,7 @@
 char *s21_strpbrk(const char *str1, const char *str2) {
   char *result = s21_NULL;
   char *temp = (char *)str1;
-  s21_size_t length = strcspn(str1, str2);
+  s21_size_t length = s21_strcspn(str1, str2);
   for (s21_size_t i = 0; i < length; i++) temp++;
   if (length) result = temp;
 
--- s21_strstr.c
+++ s21_strstr.c
@@ -4,7 +4,7 @@ char *s21_strstr(const char *haystack, const char *needle) {
   char *result = s21_NULL;
   s21_size_t length = s21_strlen(needle);
   while (*haystack) {
-    if (!memcmp(haystack++, needle, length)) {
+    if (!s21_memcmp(haystack++, needle, length)) {
       result = (char *)(haystack - 1);
       break;
     }
