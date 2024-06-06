0634bcaf2510878511a82278508176f65b38ff57
Tyrion Cassel <tyrionca@us-b3.yar.21-school.ru>
2024-06-06 19:28:51 +0300

    sscanf start implementation

--- s21_sscanf.c
+++ s21_sscanf.c
@@ -1,3 +1,16 @@
-#include "s21_string.h"
+#include "s21_sscanf.h"
 
-int s21_sscanf(const char *str, const char *format, ...) { return 0; }
+int s21_sscanf(const char *str, const char *format, ...) {
+    va_list args;
+    va_start(args);
+
+    return 0;
+}
+
+int is_space(int ch) {
+    int result = 0;
+    if (ch == ' '  || ch == '\t') {
+        result = 1;
+    }
+    return result;
+}
--- s21_sscanf.h
+++ s21_sscanf.h
@@ -1,4 +1,13 @@
 #ifndef S21_SSCANF
 #define S21_SSCANF
 
+#include <stdarg.h>
+
+typedef struct {
+    int width;
+    int lenght;
+} options;
+
+int is_space(int ch);
+
 #endif
