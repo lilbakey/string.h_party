7fdd32cc2e46d1b8e661cc0867b4b1667547378c
lilbakey <alexbakeev07@gmail.com>
2024-06-07 18:55:42 +0300

    bug fix s21_strpbrk

--- s21_strpbrk.c
+++ s21_strpbrk.c
@@ -5,7 +5,7 @@ char *s21_strpbrk(const char *str1, const char *str2) {
   char *temp = (char *)str1;
   s21_size_t length = s21_strcspn(str1, str2);
   for (s21_size_t i = 0; i < length; i++) temp++;
-  if (length) result = temp;
+  if (length != s21_strlen(str1)) result = temp;
 
   return result;
 }
