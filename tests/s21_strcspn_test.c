adf6d4491e6a776e9d0603b8fb2b4a3f326bb1fd
Sergey Savushkin <savushkin.sv@yandex.ru>
2024-06-07 18:45:47 +0300

    fix s21_strcspn_test

--- tests/s21_strcspn_test.c
+++ tests/s21_strcspn_test.c
@@ -11,6 +11,15 @@ START_TEST(test_s21_strcspn_1) {
 END_TEST
 
 START_TEST(test_s21_strcspn_2) {
+  char str1[] = "Hello, world!\n";
+  char str2[] = "Hi";
+  s21_size_t result = s21_strcspn(str1, str2);
+  s21_size_t expected = strcspn(str1, str2);
+  ck_assert_int_eq(result, expected);
+}
+END_TEST
+
+START_TEST(test_s21_strcspn_3) {
   char str1[] = "1234567890\t1234567890\n";
   char str2[] = "157";
   s21_size_t result = s21_strcspn(str1, str2);
@@ -19,7 +28,7 @@ START_TEST(test_s21_strcspn_2) {
 }
 END_TEST
 
-START_TEST(test_s21_strcspn_3) {
+START_TEST(test_s21_strcspn_4) {
   char str1[] = "Lorem ipsum dolor sit amet consectetur.\n";
   char str2[] = "_!\t\n";
   s21_size_t result = s21_strcspn(str1, str2);
@@ -28,7 +37,7 @@ START_TEST(test_s21_strcspn_3) {
 }
 END_TEST
 
-START_TEST(test_s21_strcspn_4) {
+START_TEST(test_s21_strcspn_5) {
   char str1[] = "Lorem ipsum dolor sit amet consectetur.\n";
   char str2[] = "123456";
   s21_size_t result = s21_strcspn(str1, str2);
@@ -37,7 +46,7 @@ START_TEST(test_s21_strcspn_4) {
 }
 END_TEST
 
-START_TEST(test_s21_strcspn_5) {
+START_TEST(test_s21_strcspn_6) {
   char str1[] = "";
   char str2[] = "Hello";
   s21_size_t result = s21_strcspn(str1, str2);
@@ -46,7 +55,7 @@ START_TEST(test_s21_strcspn_5) {
 }
 END_TEST
 
-START_TEST(test_s21_strcspn_6) {
+START_TEST(test_s21_strcspn_7) {
   char str1[] = "Lorem ipsum dolor sit amet consectetur.";
   char str2[] = "";
   s21_size_t result = s21_strcspn(str1, str2);
@@ -55,7 +64,7 @@ START_TEST(test_s21_strcspn_6) {
 }
 END_TEST
 
-START_TEST(test_s21_strcspn_7) {
+START_TEST(test_s21_strcspn_8) {
   char str1[] = "";
   char str2[] = "";
   s21_size_t result = s21_strcspn(str1, str2);
@@ -64,7 +73,7 @@ START_TEST(test_s21_strcspn_7) {
 }
 END_TEST
 
-START_TEST(test_s21_strcspn_8) {
+START_TEST(test_s21_strcspn_9) {
   char str1[256] = "_;,-!()*&^$#";
   char str2[12] = "abs";
   s21_size_t result = s21_strcspn(str1, str2);
@@ -83,6 +92,7 @@ TCase *tcase_s21_strcspn(void) {
   tcase_add_test(tcase, test_s21_strcspn_6);
   tcase_add_test(tcase, test_s21_strcspn_7);
   tcase_add_test(tcase, test_s21_strcspn_8);
+  tcase_add_test(tcase, test_s21_strcspn_9);
 
   return tcase;
 }
