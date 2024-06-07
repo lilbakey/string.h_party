04c3d682f85e94b77a35f4082c133241cd3b0bd2
lilbakey <alexbakeev07@gmail.com>
2024-06-08 01:30:40 +0300

    add s21_strtok func

---
+++ s21_strtok.c
@@ -0,0 +1,27 @@
+#include "s21_string.h"
+
+char *s21_strtok(char *str, const char *delim) {
+  if (delim == s21_NULL) return s21_NULL;
+
+  char *result = s21_NULL;
+  static char *string = s21_NULL;
+
+  if (str) string = str;
+
+  if (string) {
+    // skip begin delimeters
+    while (*string && !strcspn(string, delim)) string++;
+
+    if (*string != '\0') {
+      result = string;
+
+      while (*string && !strchr(delim, *string)) string++;
+
+      if (*string) {
+        *string++ = '\0';
+      } else
+        string = s21_NULL;
+    }
+  }
+  return result;
+}
